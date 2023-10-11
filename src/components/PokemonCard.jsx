f4ca4585016904027bed6879b13c3eae1704aec2
Khachik090909 <khachiksahakyan90@gmail.com>
2023-10-11 16:53:45 +0200

    corrected version

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,9 +1,26 @@
 
 function PokemonCard() {
-    return(
-    <figure><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" /></figure>,
-    <figcaption>"Bulbasaur"</figcaption>
-    );
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+  const pokemon = pokemonList[1];
+  return(
+  pokemon.imgSrc !== undefined
+    ? 
+   (<div>
+    <figure><img src={pokemon.imgSrc} alt="Bulbasaur" /></figure>,
+    <figcaption>{pokemon.name}</figcaption>
+    </div>
+    ) : <p>????</p>
+  )
   }
+
   
   export default PokemonCard;
